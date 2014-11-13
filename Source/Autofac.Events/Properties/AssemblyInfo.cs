f4fbf3cd747fb3a5af0045e65160bfa331e410e8
Jon Stelly <jonstelly@gmail.com>
2014-11-13 12:48:34 -0600

    - Add NuGet Spec

--- Source/Autofac.Events/Properties/AssemblyInfo.cs
+++ Source/Autofac.Events/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Autofac.Events")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Autofac Events extension")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jon Stelly")]
 [assembly: AssemblyProduct("Autofac.Events")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
