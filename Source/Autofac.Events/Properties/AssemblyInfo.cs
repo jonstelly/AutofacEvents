7637d34a1db5cac86178ec9b82314ae5ff511c7c
Jon Stelly <jonstelly@gmail.com>
2015-01-14 08:22:40 -0600

    First release

--- Source/Autofac.Events/Properties/AssemblyInfo.cs
+++ Source/Autofac.Events/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+[assembly: AssemblyVersion("3.5.2.*")]
+[assembly: AssemblyFileVersion("3.5.2.*")]
+[assembly: AssemblyInformationalVersion("3.5.2.0")]
