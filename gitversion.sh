ea64bc14beba41dc283c8e57adec75b5c0493856
Jon Stelly <967068+jonstelly@users.noreply.github.com>
2020-01-03 16:00:41 -0600

    mcb

--- gitversion.sh
+++ gitversion.sh
@@ -3,4 +3,7 @@
 #Expects to be run as part of github action using gitversion docker image, something like:
 #docker run -it --rm -v ${PWD}:/code --workdir /code --entrypoint ./gitversion.sh gittools/gitversion
 
-echo "::set-output name=version::$(dotnet /app/GitVersion.dll)"
+dotnet /app/GitVersion.dll > ./.gitversion.json
+#echo "::set-output name=short-sha::$(cat ./.gitversion.json | jq .ShortSha)"
+#echo "::set-output name=full-sem-ver::$(cat ./.gitversion.json | jq .FullSemVer)"
+
